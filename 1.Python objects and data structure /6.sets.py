01de707ece567c04472c81855ca16acbdb18a71d
Sbenii <sezeranben@gmail.com>
2025-09-02 23:59:58 +0200

    Add examples for basic set and boolean operations

---
+++ 1.Python objects and data structure /6.sets.py
@@ -0,0 +1,11 @@
+#Basic set operations
+myset=set()
+myset.add(1)
+myset.add(2)
+myset.add(3)
+myset.add(3)
+print(myset)
+mylist=[1,2,3,4,4,5,5,5,5,6]
+newlist=set(mylist)
+print(newlist)
+print(set('Mississipi'))
