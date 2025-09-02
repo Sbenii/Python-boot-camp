7b3923b091e571183bf219248c05e09e20274951
Sbenii <sezeranben@gmail.com>
2025-09-03 01:49:34 +0200

    Enhance file handling example by adding additional text and formatting in Python

--- 1.Python objects and data structure /8.txt.py
+++ 1.Python objects and data structure /8.txt.py
@@ -1,5 +1,7 @@
 with open("myfile.txt", "w+") as f:
     f.write("Hello this is my first text file in python!!")
+    f.write("\nMy name is Beni!!")
+    f.write("\n----------------------")
     f.seek(0)  # go back to the beginning of the file
     myfile = f.read()
     print(myfile)
