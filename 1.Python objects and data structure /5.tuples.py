b76df32042cffffeb6b2275e954f8411020f9592
Sbenii <sezeranben@gmail.com>
2025-09-02 23:47:00 +0200

    Add examples for basic tuple operations, including length, count, and index retrieval

---
+++ 1.Python objects and data structure /5.tuples.py
@@ -0,0 +1,11 @@
+#Basic tuple operations
+t=(1,2,3,4,5)
+print(len(t))
+print(t)
+t=(0,1,2,3,4,5)
+print(t)
+tple=('a','a','n',2,'z')
+print(tple.count('a'))
+print(tple.index(2))
+tp=(1,2,[1,2])#combination of list and tuple
+print(tp[2][1])
