11ee13bb5a84422679cf7876c9880b10db482fd5
Sbenii <sezeranben@gmail.com>
2025-09-02 23:28:23 +0200

    Add examples for adding and updating dictionary entries; demonstrate key, value, and item retrieval

--- 1.Python objects and data structure /4.dictionaries.py
+++ 1.Python objects and data structure /4.dictionaries.py
@@ -6,5 +6,11 @@ prices={'Apple':2.99,'Mango':2.01,'Orange':3,'Pineapple':4.81,'Watermelon':5.01}
 print(f"The price of one mango: {prices['Mango']}")
 d={'K1':123,'K2':['a','b','c'],'k3':{'k4':[4,5,6]}}
 print(f"{d['k3']['k4']}")
-
 print(f"{d['K2'][2].upper()}")
+d['k5']='Beni'
+print(f"{d}")
+d['K1']='NEW VALUE'
+print(f"{d}")
+print(f"{d.keys()}")
+print(f"{d.values()}")
+print(d.items())
