18a02b0fddb9a1c59c78514ab9de9ba4a812135c
Sbenii <sezeranben@gmail.com>
2025-09-03 17:02:37 +0200

    Add examples for iterating over dictionaries and tuples in Python

--- 3.Statements/2.forloops.py
+++ 3.Statements/2.forloops.py
@@ -16,3 +16,16 @@ for i in li:
 for (a,b) in li:
     print(a)
     print(b)
+li2=[(1,2,3),(4,5,6),(7,8,9)]
+for (a,b,c) in li2:
+    print(c)
+#Dctionaries
+d={'k1':1,'k2':2,'k3':3}
+for i in d:
+    print(i)#this will print only keys
+for i in d.values():
+    print (i)#this will print only values
+for i in d.items():
+    print(i)#this will print keys with their values
+for (a,b) in d.items():
+    print(b)#selecting what to print btn keys and values
