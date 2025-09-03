cabbb151431b432321164a11fe3e7930fdcf6452
Sbenii <sezeranben@gmail.com>
2025-09-03 17:12:27 +0000

    Sep 3, 2025, 7:12 PM

--- 3.Statements/5.listcomp.py
+++ 3.Statements/5.listcomp.py
@@ -1,3 +1,4 @@
+#For loop operators
 #Appending a list using for loop
 mystring='Hello'
 mylist=[]
@@ -8,4 +9,17 @@ print(mylist)
 mylist=[x for x in "Beni"]
 print(mylist)
 otherlist=[x for x in "Lists"]
-print(otherlist)
+print("The other list: ",otherlist)
+#Iterations operator
+num=[x for x in range(0,11)]
+print(num)
+num=[x**2 for x in range(0,11)]
+print(num)
+#Adding the if statement
+num=[x for x in range(0,11) if (x%2==0)]
+print(num)
+#Using formulas
+celcius=[0,10,20,34.5,100]
+fahrenheit=[((9/5)*temp+32) for temp in celcius]
+print(f"Temperature in celcius: {celcius}")
+print(f"Temperature in fahrenheit: {fahrenheit}")
