31faa3edd6fbf5ee05357cc4bea7198c358b2411
Sbenii <sezeranben@gmail.com>
2025-09-04 16:28:25 +0200

    Add support for keyword arguments and enhance function examples in argskwargs.py

--- 4.Methods and functions/6.argskwargs.py
+++ 4.Methods and functions/6.argskwargs.py
@@ -1,5 +1,14 @@
 #Returning 5% of the sum
+#args
 def myfunc(*args):
     return sum(args)*0.05
 sum=myfunc(1,2,545,23,12,67,88,2)
 print(f"The sum of the given numbers: {sum}")
+#kwargs
+def myfunc2(**kwargs):
+    print (kwargs)
+    if 'fruit' in kwargs:
+        print ('My choice of fruit: {}'.format(kwargs['fruit']))
+    else:
+        print('I did not find any fruit here!!!')
+myfunc2(fruit='Apple',veggie='Carbagge')
