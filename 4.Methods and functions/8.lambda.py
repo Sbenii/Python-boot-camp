defbc981b3e821adab835583f0348b2c2924b35f
Sbenii <sezeranben@gmail.com>
2025-09-05 00:00:04 +0200

    Add examples of map and filter functions in 8.lambda.py

--- 4.Methods and functions/8.lambda.py
+++ 4.Methods and functions/8.lambda.py
@@ -1,6 +1,21 @@
 #Lambda expression, map and filter expressions
+#list and map expressions
 def square(num):
     return num**2
 my_nums=[1,2,3,4,5,6]
 for i in map(square,my_nums):
     print(i)
+print(list(map(square,my_nums)))
+#example 2
+def splicer(mystr):
+    if len(mystr)%2==0:
+        return 'Even'
+    else:
+        return mystr[0]
+namee=['Beni','Andyu','Gab']
+print(list(map(splicer,namee)))
+#Filter expression
+def even(num):
+    return num%2==0
+my_nums=[1,2,3,4,5,6]
+print(list(filter(even,my_nums)))
