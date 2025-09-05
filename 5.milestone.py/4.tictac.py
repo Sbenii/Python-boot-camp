20ef7c07234253dd9313ff3fdcdbde421bb22843
Sbenii <sezeranben@gmail.com>
2025-09-05 21:00:43 +0200

    Refactor Tic Tac Toe board display and enhance game logic; update player input handling and add win check functionality

--- 5.milestone.py/4.tictac.py
+++ 5.milestone.py/4.tictac.py
@@ -1,11 +1,11 @@
 from IPython.display import clear_output
-
 def display_board(board):
     clear_output()
-    for i in range(0,9,3):
+    for i in range(1,10,3):
         print('|'+board[i] + '|' + board[i+1] + '|' + board[i+2]+'|')
         print('|-|'+'-|'+'-|')
-board = [' ']*9
+Tboard = ['X']*10
+#-----------------------------------------------
 def player_input():
     marker=''
     while marker not in ['X','O']:
@@ -16,13 +16,28 @@ def player_input():
     else:
             player2='O'
     return(player1,player2)
+#-----------------------------------------------
 def place_marker(board,marker,position):
-    while position not in range (0,9):
-        position=int(input("Enter where you want to put your marker: "))
     board[position]=marker
-    return position
-p=place_marker(board,marker='',position='')
-place_marker(board,"#",position)
-display_board(board)
-
-    
+#-----------------------------------------------
+def win_check(board,mark):
+    return ((board[1]==board[2]==board[3]==mark)or
+            (board[4]==board[5]==board[6]==mark)or
+            (board[7]==board[8]==board[9]==mark)or
+            (board[1]==board[4]==board[7]==mark)or
+            (board[2]==board[5]==board[8]==mark)or
+            (board[3]==board[6]==board[9]==mark)or
+            (board[1]==board[5]==board[9]==mark)or
+            (board[3]==board[5]==board[7]==mark))
+#-----------------------------------------------   
+import random
+def choose_first():
+    flip=random.randint(0,1)
+    if flip==0:
+        return 'Player1'
+    else:
+        return 'Player2'
+#-----------------------------------------------
+def space_check(board,position):
+    return board[position]==' '
+#-----------------------------------------------
