5b0352eadb67fb149b9b05e3d0ddd65c58f33369
Sbenii <sezeranben@gmail.com>
2025-09-06 19:23:38 +0200

    Making changes to the circle class by adding user input

--- 6.oop/3.circle.py
+++ 6.oop/3.circle.py
@@ -1,11 +1,12 @@
 class Circle():
     pi=3.14
-    def __init__(self,radius=1):
+    def __init__(self,radius):
         self.r=radius
         self.area=radius**2*Circle.pi
     def get_circumfrence(self):
         return self.r*self.pi*2
-my_circle=Circle(30)
+R=int(input("Please enter the radius of the circle: "))
+my_circle=Circle(R)
 print("Pi:{}".format(my_circle.pi))
 print("Radius: {}".format(my_circle.r))
 print("Circumfrence: {}".format(my_circle.get_circumfrence()))
