d976c7282a9bb8de27b1a5f058542722b17ea86d
Sbenii <sezeranben@gmail.com>
2025-09-09 00:01:21 +0200

    Add functionalities to the generator file

---
+++ 12.Python generators/generators.py
@@ -0,0 +1,38 @@
+def create_cubes(n):
+    for i in range (n):
+        yield (i**3)#----> We use yield as our generator!!
+for x in create_cubes(10):
+    print (x)
+print(list(create_cubes(10)))
+#-------------------------------------------------
+def fibon(n):
+    a=1
+    b=1
+    for x in range(n):
+        yield a
+        a,b=b,a+b
+for number in fibon(10):
+    print(number)
+#--------------------------------------------------
+def simple_gen():
+    for x in range(3):
+        yield x
+for i in simple_gen():
+    print (i)
+    
+g=simple_gen()
+print(next(g))
+print(next(g))
+print(next(g))
+#---------------------------------------------------
+word='Hello'
+for x in word:
+    print(x)
+word_iter=iter(word)
+print(next(word_iter))
+print(next(word_iter))
+print(next(word_iter))
+print(next(word_iter))
+print(next(word_iter))
+print(next(word_iter))
+
