1c80c12374d3ed751d057e7bc841723dc31d0817
Sbenii <sezeranben@gmail.com>
2025-09-09 22:01:09 +0200

    Add functionalities and examples to the counter file

--- 13.Advanced Python modules/1.counter.py
+++ 13.Advanced Python modules/1.counter.py
@@ -1,3 +1,9 @@
 from collections import Counter
 my_list=[1,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4]
 print(Counter(my_list))
+my_list=['a','a','a',1,1,1,1,'b','b']
+print(Counter(my_list))
+mylist=('aaaaaaaaabbbbbannnnnaaaiiiiiaoooo')
+print(Counter(mylist))
+sentence='How many times does each word comes in this sentence?'
+print(Counter(sentence.split()))
