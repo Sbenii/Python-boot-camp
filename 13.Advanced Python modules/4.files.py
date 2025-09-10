7b4078fca7596a55964a02a72c3a5099c186ca0d
Sbenii <sezeranben@gmail.com>
2025-09-10 21:11:58 +0200

    Move the Text.txt file using shutil.move function

--- 13.Advanced Python modules/4.files.py
+++ 13.Advanced Python modules/4.files.py
@@ -2,5 +2,7 @@ t=open('Test.txt','w+')
 t.write("This is a test string!!")
 t.close()
 import os
-print(os.getcwd())
-print(os.listdir())
+print(os.getcwd())#-->shows where you are working in
+print(os.listdir())#-->list all things in a given directory
+import shutil
+shutil.move('Test.txt','/users/beni/documents/skills/Machine Learning/Python/Python boot camp/13.Advanced Python modules')
