faee77929b9342d2c843debcab63a763e3b4ba42
Sbenii <sezeranben@gmail.com>
2025-09-10 20:41:21 +0200

    Add most common and list function

--- 13.Advanced Python modules/1.counter.py
+++ 13.Advanced Python modules/1.counter.py
@@ -4,7 +4,10 @@ my_list=[1,1,1,1,1,1,2,2,2,2,3,3,3,3,3,4,4,4]
 print(Counter(my_list))
 my_list=['a','a','a',1,1,1,1,'b','b']
 print(Counter(my_list))
-mylist=('aaaaaaaaabbbbbannnnnaaaiiiiiaoooo')
-print(Counter(mylist))
 sentence='How many times does each word comes in this sentence?'
 print(Counter(sentence.split()))
+mylist=('aaaaaaaaabbbbbannnnnaaaiiiiiaoooo')
+c=Counter(mylist)
+print(c)
+print(c.most_common(2))#---->returns the most recurring item
+print(list(c))
