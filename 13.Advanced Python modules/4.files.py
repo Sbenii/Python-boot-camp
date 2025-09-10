c02c2e97d377cc2991efbc9c6839074c2fe21eaa
Sbenii <sezeranben@gmail.com>
2025-09-10 21:16:17 +0200

    Install send2trash library

--- 13.Advanced Python modules/4.files.py
+++ 13.Advanced Python modules/4.files.py
@@ -5,4 +5,4 @@ import os
 print(os.getcwd())#-->shows where you are working in
 print(os.listdir())#-->list all things in a given directory
 import shutil
-shutil.move('Test.txt','/users/beni/documents/skills/Machine Learning/Python/Python boot camp/13.Advanced Python modules')
+shutil.move('/users/beni/documents/skills/Machine Learning/Python/Test.txt',os.getcwd())
