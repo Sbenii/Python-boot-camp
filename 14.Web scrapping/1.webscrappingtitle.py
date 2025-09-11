5f97f7a3ac2c75b2eba23bd4c301f3ff96423b11
Sbenii <sezeranben@gmail.com>
2025-09-11 22:04:59 +0200

    Add a webscrapper script to display title and paragraphs of a given site

--- 14.Web scrapping/1.webscrappingtitle.py
+++ 14.Web scrapping/1.webscrappingtitle.py
@@ -1,2 +1,14 @@
 import requests
+result=requests.get("https://www.example.com/")
+'''
+print(type(result))
+print(result.text)
+'''
+
+
 import bs4
+soup=bs4.BeautifulSoup(result.text,'lxml')
+print(soup)
+print(soup.select('title')[0].getText())
+site_paragraphs=soup.select('p')
+print(site_paragraphs[0].getText())
