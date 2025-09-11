14c189cc0649aa1f01c1fca9910a6616ebcc6200
Sbenii <sezeranben@gmail.com>
2025-09-11 09:43:39 +0200

    Add re library

--- 13.Advanced Python modules/10.regularexpressions.py
+++ 13.Advanced Python modules/10.regularexpressions.py
@@ -11,3 +11,5 @@ myli="My phone, my phone where is you my phone??"
 matches=re.findall('phone',myli)
 print(matches)
 print(len(matches))
+for matches in re.finditer(pattern,myli):
+    print(matches)
