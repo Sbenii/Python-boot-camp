e1d07c3369495ed01c6f4c3ff2c178af3556ffae
Sbenii <sezeranben@gmail.com>
2025-09-11 11:56:49 +0200

    Import the zip library and zip and unzip files

--- 13.Advanced Python modules/15.zip.py
+++ 13.Advanced Python modules/15.zip.py
@@ -1,3 +1,16 @@
 f=open('Fileone.txt','w+')
 f.write("This is the first text!!")
 f.close()
+
+f=open('Filetwo.txt','w+')
+f.write("This is the second text!!")
+f.close()
+
+import zipfile
+comp_file=zipfile.ZipFile("Compressed_file.zip","w")
+comp_file.write('Fileone.txt',compress_type=zipfile.ZIP_DEFLATED)
+comp_file.write('Filetwo.txt',compress_type=zipfile.ZIP_DEFLATED)
+comp_file.close()
+
+zip_obj=zipfile.ZipFile('Compressed_file.zip','r')
+zip_obj.extractall('Extracted_content')
