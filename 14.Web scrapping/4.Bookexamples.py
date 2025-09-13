6593559e2e0fa6577cb9589c0ec6678ee37ff875
Sbenii <sezeranben@gmail.com>
2025-09-13 11:49:18 +0200

    Add request and beautiful soup functionalities to get the class of each product

--- 14.Web scrapping/4.Bookexamples.py
+++ 14.Web scrapping/4.Bookexamples.py
@@ -1,3 +1,9 @@
 #Get a title of a book with a two star rating on books.toscrape.com
 import requests
 import bs4
+base_url='https://books.toscrape.com/catalogue/page-{}.html'
+page_number=12
+print(base_url.format(page_number))
+res=requests.get(base_url.format(1))
+soup=bs4.BeautifulSoup(res.text,'lxml')
+print(len(soup.select('.product_pod')))
