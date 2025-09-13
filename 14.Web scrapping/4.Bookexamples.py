5790323a3b390459442823386367f382a01ec873
Sbenii <sezeranben@gmail.com>
2025-09-13 11:57:46 +0200

    Add select method to get rating class and title of the product

--- 14.Web scrapping/4.Bookexamples.py
+++ 14.Web scrapping/4.Bookexamples.py
@@ -6,4 +6,9 @@ page_number=12
 print(base_url.format(page_number))
 res=requests.get(base_url.format(1))
 soup=bs4.BeautifulSoup(res.text,'lxml')
-print(len(soup.select('.product_pod')))
+products=(soup.select('.product_pod'))
+example=products[0]
+print(example.select('.star-rating.Three'))
+print(example.select('a'))
+print(example.select('a')[1])
+print(example.select('a')[1]['title'])
