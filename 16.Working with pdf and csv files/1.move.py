e74327e06c44eddbbb511f1b4fbd394d8bfd06be
Sbenii <sezeranben@gmail.com>
2025-09-14 18:52:53 +0200

    Add some programs

---
+++ 16.Working with pdf and csv files/1.move.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/makeup_new.csv',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/Some_BrandNew_Doc.pdf',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/Some_New_Doc.pdf',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/Some_new_file.pdf',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/to_save_file.csv',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/Working_Business_Proposal.pdf',os.getcwd())
+shutil.move('/Users/beni/Documents/Skills/Machine Learning/Python/Complete-Python-3-Bootcamp-master/15-PDFs-and-Spreadsheets/example.csv',os.getcwd())
