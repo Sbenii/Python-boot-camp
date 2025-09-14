38b489c202d6083354b5765dd136d413520a7374
Sbenii <sezeranben@gmail.com>
2025-09-14 18:57:06 +0200

    Add advanced strings program

---
+++ 18.Advanced python objects and data structures/2.advancedstrings.py
@@ -0,0 +1,15 @@
+
+s='hello world'
+print(s.capitalize())
+print(s.upper())
+print(s.lower())
+print(s.count('o'))
+print(s.find('o'))
+print(s.center(20,'b'))
+b='Beni'
+print(b.isalnum())
+print(b.isalpha())
+print(b.islower())
+print(b.isspace())
+print(b.endswith('i'))
+print(b.split('e'))
